05c11c81bfed4c98024f20bd591c45f013561651
Charles Bissonnette <chabiss@microsoft.com>
2023-03-22 17:58:41 -0700

    Add Detail pane

---
+++ PeriodicTableNET/PeriodicTableBlazorMaui/Shared/SelectedElementInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeriodicTableBlazorMaui.Shared
+{
+    public class SelectedElementInfo
+    {
+        public SelectedElementInfo(Action callback)
+        {
+            SelectedElementChanged = callback;
+        }
+        Action SelectedElementChanged;
+        public PeriodicTableData.Element? SelectedElement { get; set; }
+        public void SetActiveElement(PeriodicTableData.Element element)
+        {
+            this.SelectedElement = element;
+            this.SelectedElementChanged();
+        }
+    }
+}
